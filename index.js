019132efb9a36497a347c5b3ae7139e2fb5c3827
Lebang <lebangnong@gmail.com>
2023-08-26 18:01:08 +0200

    Req and res, Nodemon and port function

--- index.js
+++ index.js
@@ -16,6 +16,11 @@ app.get('/api/courses', (req,res) => {
     res.send([1, 2, 3]);
 })
 
-app.listen(3000, () => {
-    console.log('Listening to port 3000...')
+app.get('/api/courses/:id', (req, res) => {
+    res.send(req.param.id);
+})
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log(`Listening to port ${port}...`)
 });
